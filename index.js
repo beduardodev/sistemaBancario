42b24eb7f5fcced48809fe3a01fdbf0376900aed
BEduardo <beduardo.dev@gmail.com>
2021-03-25 22:02:27 -0300

    Criando classe de conta corrente e efetuando saque

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 class Cliente {
     nome;
     cpf;
+}
+class ContaCorrente {
     agencia;
     saldo;
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
 
 cliente1.nome = "Bruno Eduardo";
 cliente1.cpf = 12172931575;
-cliente1.agencia = 3671;
-cliente1.saldo = 0;
+
+const cliente2 = new Cliente();
 
 cliente2.nome = "Bruce Wayne";
 cliente2.cpf = 12187931575;
-cliente2.agencia = 3656;
-cliente2.saldo = 0;
+
+const contaCorrente1 = new ContaCorrente();
+
+contaCorrente1.agencia = 11561;
+contaCorrente1.saldo = 0;
+
+console.log(contaCorrente1.saldo);
+contaCorrente1.saldo = 100;
+console.log(contaCorrente1.saldo);
+
+let valorSacado = 200;
+
+if (contaCorrente1.saldo >= valorSacado) {
+    contaCorrente1.saldo -= valorSacado;
+}
+
+console.log(contaCorrente1.saldo);
 
 console.log(cliente1);
 console.log(cliente2);
