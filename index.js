26c3b1eaa1f0ecf736a201c78665f6ad2cf62c5e
BEduardo <beduardo.dev@gmail.com>
2021-03-28 16:01:36 -0300

    Implementando metodos de transferir

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ cliente2.nome = "Bruce Wayne";
 cliente2.cpf = 12187931575;
 
 const contaCorrente1 = new ContaCorrente();
+const contaCorrente2 = new ContaCorrente();
 
 contaCorrente1._saldo = 0;
 contaCorrente1.agencia = 16158;
+contaCorrente1.cliente = cliente1;
+contaCorrente1.depositar(500);
+
+contaCorrente2._saldo = 0;
+contaCorrente2.agencia = 156168;
+contaCorrente2.cliente = cliente2;
+
+contaCorrente1.transferir(200, contaCorrente2);
+
+console.log(contaCorrente1);
+console.log(contaCorrente2);
