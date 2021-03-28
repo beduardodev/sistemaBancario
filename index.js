329706ddb5f7cc850b303f42b1095aaf9cc46d6f
BEduardo <beduardo.dev@gmail.com>
2021-03-28 20:21:54 -0300

    Adicionando aviso para que o método Conta nao seja utilizado diretamente

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Cliente } from "./models/Cliente.js";
+import { Conta } from "./models/Conta.js";
 import { ContaCorrente } from "./models/ContaCorrente.js";
 import { ContaPoupanca } from "./models/ContaPoupanca.js";
 
@@ -8,6 +9,10 @@ const contaCorrente1 = new ContaCorrente(cliente1, 741258963);
 contaCorrente1.depositar(500);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 741258963, 1001);
+
+const conta = new Conta(0, cliente1, 1001);
+
+console.log(conta);
 
 console.log(contaPoupanca);
 console.log(contaCorrente1);
