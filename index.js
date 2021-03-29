78ef9a2086f05ae488a6b1a1dbb2cbdd84f3591c
BEduardo <beduardo.dev@gmail.com>
2021-03-29 20:17:24 -0300

    Ajuste no import, testando metodo de login

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+import { SistemaAutenticacao } from "./controlers/SistemaAutenticacao.js";
 import { Cliente } from "./models/Cliente.js";
 import { Diretor } from "./models/Funcionarios/Diretor.js";
 import { Gerente } from "./models/Funcionarios/Gerente.js";
 
 
 const diretor = new Diretor("Bruce Wayne", 10000, 568713554125);
-const gerente = new Gerente("Dick Grayson", 5000, 1651654164168);
+diretor.cadastrarSenha("741258963");
 
-SistemaAutenticacao.login(diretor, "741258963");
+const gerente = new Gerente("Dick Grayson", 5000, 1651654164168);
+gerente.cadastrarSenha("123");
+
+const estaLogado = SistemaAutenticacao.login(gerente, "123");
+
+console.log(estaLogado);
